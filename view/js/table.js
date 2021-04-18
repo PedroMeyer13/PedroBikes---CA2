8a94c9a2d8b75aaa4c477d49102375dcaed94039
PedroMeyer13 <pedro_manchaverde13@hotmail.com>
2021-04-18 22:46:46 +0000

    Get method fix

--- view/js/table.js
+++ view/js/table.js
@@ -1,22 +1,49 @@
-function draw_table()
-{
-	fetch('https://8000-aqua-turtle-q66rq7nx.ws-eu03.gitpod.io/bikes')
-  .then(res => {
-    if (res.ok){
-      console.log('Sucess')
-    } else {
-      console.log('Failiure')
-    }
-  })
-  .then(data => console.log(data))
-  .catch(error => console.log('Error'))
-};
+  // api url
+const api_url = 'https://8000-aqua-turtle-q66rq7nx.ws-eu03.gitpod.io/bikes';
+console.log("Here");
+// Defining async function
+async function getapi(url) {
+  
 
+// Storing response
+const response = await fetch(url);
 
-$(document).ready(function ()
-{
-	
-});
+// Storing data in form of JSON
+var data = await response.json();
+console.log(data);
+if (response) {
+  hideloader();
+}
+show(data);
+}
+// Calling that async function
+getapi(api_url);
 
+// Function to hide the loader
+function hideloader() {
+document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+  console.log(data + "Here");
+let tab = 
+  `<tr>
+    <th>bikes</th>
+    <th>kids</th>
+    <th>item</th>
+    <th>price</th>
+   </tr>`;
 
-
+// Loop to access all rows 
+for (let r of data.list) {
+  tab += `<tr> 
+<td>${r.bikes} </td>
+<td>${r.kids}</td>
+<td>${r.item}</td> 
+<td>${r.price}</td>          
+</tr>`;
+}
+// Setting innerHTML as tab variable
+document.getElementById("employees").innerHTML = tab;
+console.log(data + "Here");
+}
